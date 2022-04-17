5538d02ddd11a2c9da586a8dbc69f8d1a1e347a1
em432 <emejeothniel@gmail.com>
2022-04-17 02:56:32 +0100

    Update print_custom.c

--- print_custom.c
+++ print_custom.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_bigS - Non printable characters
@@ -12,29 +12,29 @@
  */
 int print_bigS(va_list l, flags_t *f)
 {
-    int i, count = 0;
-    char *res;
-    char *s = va_arg(l, char *);
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
 
-    (void)f;
-    if (!s)
-        return (_puts("(null)"));
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
 
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-        {
-            _puts("\\x");
-            count += 2;
-            res = convert(s[i], 16, 0);
-            if (!res[1])
-                count += _putchar('0');
-            count += _puts(res);
-        }
-        else
-            count += _putchar(s[i]);
-    }
-    return (count);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+	}
+	return (count);
 }
 
 /**
@@ -46,20 +46,20 @@ int print_bigS(va_list l, flags_t *f)
  */
 int print_rev(va_list l, flags_t *f)
 {
-    int i = 0, j;
-    char *s = va_arg(l, char *);
+	int i = 0, j;
+	char *s = va_arg(l, char *);
 
-    (void)f;
-    if (!s)
-        s = "(null)";
+	(void)f;
+	if (!s)
+		s = "(null)";
 
-    while (s[i])
-        i++;
+	while (s[i])
+		i++;
 
-    for (j = i - 1; j >= 0; j--)
-        _putchar(s[j]);
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
 
-    return (i);
+	return (i);
 }
 
 /**
@@ -71,27 +71,27 @@ int print_rev(va_list l, flags_t *f)
  */
 int print_rot13(va_list l, flags_t *f)
 {
-    int i, j;
-    char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-    char *s = va_arg(l, char *);
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(l, char *);
 
-    (void)f;
-    for (j = 0; s[j]; j++)
-    {
-        if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
-            _putchar(s[j]);
-        else
-        {
-            for (i = 0; i <= 52; i++)
-            {
-                if (s[j] == rot13[i])
-                    _putchar(ROT13[i]);
-            }
-        }
-    }
+	(void)f;
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
 
-    return (j);
+	return (j);
 }
 
 /**
@@ -102,7 +102,7 @@ int print_rot13(va_list l, flags_t *f)
  */
 int print_percent(va_list l, flags_t *f)
 {
-    (void)l;
-    (void)f;
-    return (_putchar('%'));
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
 }
